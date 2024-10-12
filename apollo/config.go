44529614223e133b74f24260ea4e21b8d3585978
no_f <jiangbenli1965@gmail.com>
2024-10-12 11:29:47 +0800

    perf(代码优化):

--- apollo/config.go
+++ apollo/config.go
@@ -6,6 +6,7 @@ import (
 	"github.com/apolloconfig/agollo/v4/env/config"
 	"go-base/apollo/info"
 	"go-base/apollo/model"
+	"log"
 	"strings"
 )
 
@@ -15,89 +16,89 @@ var apolloClientV2 agollo.Client
 var allNamespaceNameV1 string
 var allNamespaceNameV2 string
 
+var namespacesV1 []string
+var namespacesV2 []string
+
 // init 初始化
 func init() {
 	apolloInitV1()
 	apolloInitV2()
+
+	namespacesV1 = strings.Split(allNamespaceNameV1, ",")
+	namespacesV2 = strings.Split(allNamespaceNameV2, ",")
+}
+
+func initApolloClient(namespaceName string, appID string) agollo.Client {
+	apolloConfig, err := model.LoadConfig()
+	if err != nil {
+		log.Fatalf("LoadConfig失败: %v", err)
+	}
+
+	c := &config.AppConfig{
+		AppID:          appID,
+		Cluster:        apolloConfig.Apollo.Cluster,
+		IP:             apolloConfig.Apollo.Meta,
+		NamespaceName:  namespaceName,
+		IsBackupConfig: true,
+	}
+	client, err := agollo.StartWithConfig(func() (*config.AppConfig, error) {
+		return c, nil
+	})
+	if err != nil {
+		log.Fatalf("StartWithConfig失败: %v", err)
+	}
+
+	return client
 }
 
 func apolloInitV1() {
 	apolloConfig, err := model.LoadConfig()
 	if err != nil {
 		fmt.Printf("LoadConfig失败: %v", err)
+		panic("无法加载配置")
 	}
-
 	allNamespaceNameV1 = info.Namespace + apolloConfig.Apollo.CommonNamespaceName
-	c := &config.AppConfig{
-		AppID:          "bullyun-v2",
-		Cluster:        apolloConfig.Apollo.Cluster,
-		IP:             apolloConfig.Apollo.Meta,
-		NamespaceName:  allNamespaceNameV1,
-		IsBackupConfig: true,
-	}
-	apolloClientV1, err = agollo.StartWithConfig(func() (*config.AppConfig, error) {
-		return c, nil
-	})
+	apolloClientV1 = initApolloClient(allNamespaceNameV1, "bullyun-v2")
 }
 
-// apolloInit 初始化 apollo 客户端
 func apolloInitV2() {
 	apolloConfig, err := model.LoadConfig()
 	if err != nil {
 		fmt.Printf("LoadConfig失败: %v", err)
+		panic("无法加载配置")
 	}
-
 	allNamespaceNameV2 = "application," + apolloConfig.Apollo.NamespaceName
-	c := &config.AppConfig{
-		AppID:          apolloConfig.Apollo.AppID,
-		Cluster:        apolloConfig.Apollo.Cluster,
-		IP:             apolloConfig.Apollo.Meta,
-		NamespaceName:  allNamespaceNameV2,
-		IsBackupConfig: true,
-	}
-	apolloClientV2, err = agollo.StartWithConfig(func() (*config.AppConfig, error) {
-		return c, nil
-	})
+	apolloClientV2 = initApolloClient(allNamespaceNameV2, apolloConfig.Apollo.AppID)
 }
 
 // GetConfigValue 根据给定的 key 获取配置值
 func GetConfigValue(key string) string {
-	if apolloClientV1 == nil {
-		return ""
+	value := getConfigValueFromClient(apolloClientV1, namespacesV1, key)
+	if value != "" {
+		return value
 	}
+	return getConfigValueFromClient(apolloClientV2, namespacesV2, key)
+}
 
-	for _, ns := range strings.Split(allNamespaceNameV1, ",") {
-		cache := apolloClientV1.GetConfigCache(ns)
-
+func getConfigValueFromClient(client agollo.Client, namespaces []string, key string) string {
+	for _, ns := range namespaces {
+		cache := client.GetConfigCache(ns)
 		if cache == nil {
+			log.Printf("Namespace cache not found for: %s", ns)
 			continue
 		}
 		value, err := cache.Get(key)
-		if err != nil {
-			continue
+		if err == nil {
+			return toString(value)
 		}
-		return toString(value)
-	}
-
-	for _, ns := range strings.Split(allNamespaceNameV2, ",") {
-		cache := apolloClientV2.GetConfigCache(ns)
-
-		if cache == nil {
-			continue
-		}
-		value, err := cache.Get(key)
-		if err != nil {
-			continue
-		}
-		return toString(value)
+		log.Printf("Failed to get config value for key %s in namespace %s: %v", key, ns, err)
 	}
 	return ""
 }
 
 func toString(value interface{}) string {
-	str, ok := value.(string)
-	if !ok {
-		return ""
+	if str, ok := value.(string); ok {
+		return str
 	}
-	return str
+	return ""
 }
